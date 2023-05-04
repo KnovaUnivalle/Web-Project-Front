310ea0767e8d1eb22877bfe9c1cfdadf465acbe2
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2023-05-03 23:20:01 -0500

    empty pages, add not found page

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,12 +1,16 @@
 import { Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
+import LandingPage from '../pages/LandingPage';
+import NotFound from '../pages/NotFound';
+import Home from '../pages/Home';
 
 const AppRoutes = () => {
 	return (
 		<>
 			<Routes>
-				<Route path='/*' element={<div>Home</div>} />
-				<Route path='/land*' element={<div>Landing Page</div>} />
+				<Route path='*' element={<NotFound />} />
+				<Route path='/' element={<Home />} />
+				<Route path='/land' element={<LandingPage />} />
 			</Routes>
 		</>
 	);
