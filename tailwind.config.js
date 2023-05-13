9941b3429a88d6f7c5ddef58e32217bfc84002d1
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2023-05-13 17:57:35 -0500

    add colors tailwind, add letter logo, change logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				softlime: '#6EB500',
+				softblue: '#104BA9',
+				softorange: '#FFA200',
+				softgrey: '#f2f3f5',
+			},
+		},
+	},
 
+	plugins: [],
+};
