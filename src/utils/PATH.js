2160e10aabce7bc451b13dab6cba74c7dce0d1ba
Franklyn_Narvaez <franklyn.narvaez@correounivalle.edu.co>
2023-05-26 15:04:31 -0500

    add routes n path

--- src/utils/PATH.js
+++ src/utils/PATH.js
@@ -1,9 +1,11 @@
 // App Router
 export const LANDING_PAGE_PATH = '/';
 export const LANDING_PAGE_AS_PATH = '/*';
+export const SIGN_AS_PATH = 'sign/*';
 export const HOME_PATH = '/home';
 export const HOME_AS_PATH = 'home/*';
-export const SIGN_AS_PATH = 'sign/*';
+export const MANAGER_HOME_AS_PATH = 'manager/*';
+export const ADMIN_HOME_AS_PATH = 'admin/*';
 
 // Land Router
 export const LAND_TEAM_PATH = '/team';
