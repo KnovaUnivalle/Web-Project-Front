3da5df7910800f67db440f025cf46c57d4b16b4e
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2023-05-28 16:06:23 -0500

    initial admiin y manager page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,5 +1,22 @@
+import NavBarOperations from '../components/navbar/NavBarOperations';
+import PersonIcon from '@mui/icons-material/Person';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import LogoutIcon from '@mui/icons-material/Logout';
+import HomeIcon from '@mui/icons-material/Home';
+
+const menus = [
+	{ title: 'Inicio', icon: <HomeIcon />, url: '', divider: true },
+	{ title: 'Usuarios', icon: <PersonIcon />, url: '', divider: false },
+	{ title: 'Agregar Usuario', icon: <PersonAddAlt1Icon />, url: '', divider: false },
+	{ title: 'Modificar Usuario', icon: <ManageAccountsIcon />, url: '', divider: true },
+	{ title: 'Gráficos', icon: <BarChartIcon />, url: '', divider: true },
+	{ title: 'Cerrar Sesión', icon: <LogoutIcon />, url: '', divider: true },
+];
+
 const Admin = () => {
-	return <div>Admin</div>;
+	return <NavBarOperations body={<div> Admin</div>} menus={menus} title='Administrador' />;
 };
 
 export default Admin;
--- src/pages/Manager.jsx
+++ src/pages/Manager.jsx
@@ -1,5 +1,18 @@
+import NavBarOperations from '../components/navbar/NavBarOperations';
+import PersonIcon from '@mui/icons-material/Person';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import LogoutIcon from '@mui/icons-material/Logout';
+import HomeIcon from '@mui/icons-material/Home';
+
+const menus = [
+	{ title: 'Inicio', icon: <HomeIcon />, url: '', divider: true },
+	{ title: 'Usuarios', icon: <PersonIcon />, url: '', divider: true },
+	{ title: 'Gráficos', icon: <BarChartIcon />, url: '', divider: true },
+	{ title: 'Cerrar Sesión', icon: <LogoutIcon />, url: '', divider: true },
+];
+
 const Manager = () => {
-	return <div>Manager</div>;
+	return <NavBarOperations body={<div>Manager</div>} menus={menus} title='Administrador' />;
 };
 
 export default Manager;
